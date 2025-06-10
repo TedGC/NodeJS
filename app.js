d1bdfdaf2c936ea45568191bb7b438af7fc3017e
TedGC <rhkdtn2056@gmail.com>
2025-06-10 21:46:46 +0900

    update

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ const fs = require('fs')
 
 fs.appendFileSync('notes.txt', ' I live in Philadelphia.')
 
-
+// read a file asynchronously 
 
 const fs = require('fs');
 
 fs.readFile('./data.txt', 'utf8', (err, data) => {
     if (err) throw err;
     console.log(data);
+});
+
+//write to file
+fs.writeFile('./output.txt', 'Hello Node.js!', err => {
+    if (err) throw err;
+    console.log('File written!');
 });
