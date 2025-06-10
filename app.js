976d6bd049f9a1db5d9fa283e97e8bf4b7817509
TedGC <rhkdtn2056@gmail.com>
2025-06-10 21:46:14 +0900

    update

--- app.js
+++ app.js
@@ -3,3 +3,12 @@ const fs = require('fs')
 // fs.writeFileSync('notes.txt', 'My name is Andrew.')
 
 fs.appendFileSync('notes.txt', ' I live in Philadelphia.')
+
+
+
+const fs = require('fs');
+
+fs.readFile('./data.txt', 'utf8', (err, data) => {
+    if (err) throw err;
+    console.log(data);
+});
