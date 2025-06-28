837170ad9e2a37169227f325153218d32fc70e5f
TedGC <rhkdtn2056@gmail.com>
2025-06-28 19:59:05 +0900

    update

--- OnlineMallPR/practice.js
+++ OnlineMallPR/practice.js
@@ -151,3 +151,12 @@ function memoize(fn) {
         return result;
     };
 }
+
+//currying a function 
+function curry(fn) {
+    return function curried(...args) {
+        return args.length >= fn.length
+            ? fn.apply(this, args)
+            : (...next) => curried.apply(this, args.concat(next));
+    };
+}
