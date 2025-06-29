cdfb47262bd873740bdb5bdbaa76f0c5783af7d4
TedGC <rhkdtn2056@gmail.com>
2025-06-29 18:39:41 +0900

    udpate

--- OnlineMallPR/practice.js
+++ OnlineMallPR/practice.js
@@ -259,3 +259,18 @@ const loggedUser = new Proxy(user, {
 });
 
 console.log(loggedUser.name); // logs "Accessed name", then outputs "Ted"
+
+//memorization with closure 
+
+function memoize(fn) {
+    const cache = {};
+    return function (...args) {
+        const key = args.toString();
+        if (cache[key]) return cache[key];
+        cache[key] = fn(...args);
+        return cache[key];
+    };
+}
+
+const factorial = memoize(n => (n <= 1 ? 1 : n * factorial(n - 1)));
+console.log(factorial(5)); // 120
