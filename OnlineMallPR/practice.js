f9c62863c163b08876b3378d0c342377c1ed0f2a
TedGC <rhkdtn2056@gmail.com>
2025-06-30 21:05:40 +0900

    update

--- OnlineMallPR/practice.js
+++ OnlineMallPR/practice.js
@@ -458,3 +458,19 @@ function debounce(fn, delay, immediate = false) {
 // Usage
 const log = debounce(() => console.log("Debounced!"), 1000, true);
 log(); log(); log(); // Only first call trigger
+
+
+// custom deep clone 
+function deepClone(obj, hash = new WeakMap()) {
+    if (obj === null || typeof obj !== 'object') return obj;
+    if (hash.has(obj)) return hash.get(obj);
+
+    const clone = Array.isArray(obj) ? [] : {};
+    hash.set(obj, clone);
+
+    for (let key in obj) {
+        clone[key] = deepClone(obj[key], hash);
+    }
+
+    return clone;
+}
