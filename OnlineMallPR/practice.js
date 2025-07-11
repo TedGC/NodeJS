748f39678cc848ae9b638adbead1dec3644c95eb
TedGC <rhkdtn2056@gmail.com>
2025-07-11 21:47:16 +0900

    update

--- OnlineMallPR/practice.js
+++ OnlineMallPR/practice.js
@@ -1002,3 +1002,13 @@ axios.get('https://api.github.com/users/octocat')
     .catch(error => {
         console.error(error);
     });
+
+
+
+const p1 = Promise.resolve(1);
+const p2 = Promise.resolve(2);
+const p3 = Promise.reject('fail');
+
+Promise.all([p1, p2, p3])
+    .then(results => console.log(results))
+    .catch(err => console.error('Error:', err));
