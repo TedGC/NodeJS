6a76cee4b738d98921390d4fc459730b2b6ff46b
TedGC <rhkdtn2056@gmail.com>
2025-07-16 22:43:09 +0900

    update

--- OnlineMallPR/practice.js
+++ OnlineMallPR/practice.js
@@ -1547,3 +1547,11 @@ for (const num of range) {
         }
         return cloned;
     }
+
+
+    function sum(a) {
+        return function (b) {
+            return b ? sum(a + b) : a;
+        };
+    }
+    console.log(sum(1)(2)(3)(4)()); // 10
