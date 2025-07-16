1d0844c7d87668c13ce3100b2c188b06ad669571
TedGC <rhkdtn2056@gmail.com>
2025-07-16 22:43:00 +0900

    update

--- OnlineMallPR/practice.js
+++ OnlineMallPR/practice.js
@@ -1536,3 +1536,14 @@ for (const num of range) {
 
     const name = 'Ted';
     console.log(bold`Hello, ${name}!`);
+
+
+    function deepClone(obj) {
+        if (obj === null || typeof obj !== 'object') return obj;
+        if (Array.isArray(obj)) return obj.map(deepClone);
+        const cloned = {};
+        for (const key in obj) {
+            cloned[key] = deepClone(obj[key]);
+        }
+        return cloned;
+    }
