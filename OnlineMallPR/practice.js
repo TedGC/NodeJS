a5481b49b333afb8a4debb6fc5fa10db67a214ec
TedGC <rhkdtn2056@gmail.com>
2025-07-31 21:12:15 +0900

    update

--- OnlineMallPR/practice.js
+++ OnlineMallPR/practice.js
@@ -2369,3 +2369,18 @@ for (const num of range) {
         const emitter = new EventEmitter();
         emitter.on('greet', name => console.log(`Hello, ${name}!`));
         emitter.emit('greet', 'Alice');
+
+
+        function* fibonacci() {
+            let a = 0, b = 1;
+            while (true) {
+                yield a;
+                [a, b] = [b, a + b];
+            }
+        }
+
+        const gen = fibonacci();
+        console.log(gen.next().value); // 0
+        console.log(gen.next().value); // 1
+        console.log(gen.next().value); // 1
+        console.log(gen.next().value); // 2
