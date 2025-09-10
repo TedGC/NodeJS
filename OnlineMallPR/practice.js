55c04361e1b4891a81bcdeaf1bf269a9d95aa664
TedGC <rhkdtn2056@gmail.com>
2025-09-10 22:04:09 +0900

    udpate

--- OnlineMallPR/practice.js
+++ OnlineMallPR/practice.js
@@ -2833,3 +2833,14 @@ for (const num of range) {
                 }
 
                 getData();
+
+
+
+                const EventEmitter = require('events');
+                const emitter = new EventEmitter();
+
+                emitter.on('greet', (name) => {
+                    console.log(`Hello ${name}!`);
+                });
+
+                emitter.emit('greet', 'Alice');
