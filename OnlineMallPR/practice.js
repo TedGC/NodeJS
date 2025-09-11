8e866fa18869b89ef57d5118369eb42627c7b7b7
TedGC <rhkdtn2056@gmail.com>
2025-09-11 22:31:02 +0900

    update

--- OnlineMallPR/practice.js
+++ OnlineMallPR/practice.js
@@ -2876,3 +2876,18 @@ for (const num of range) {
 
                     return debounced;
                 }
+
+
+
+                import { useState, useEffect } from "react";
+
+                function useDebounce(value, delay = 500) {
+                    const [debounced, setDebounced] = useState(value);
+
+                    useEffect(() => {
+                        const handler = setTimeout(() => setDebounced(value), delay);
+                        return () => clearTimeout(handler);
+                    }, [value, delay]);
+
+                    return debounced;
+                }
