1c2add6fbdf00bbbab943ee1eee29bd87d22ccf2
TedGC <rhkdtn2056@gmail.com>
2025-09-11 22:32:25 +0900

    update

--- OnlineMallPR/practice.js
+++ OnlineMallPR/practice.js
@@ -2936,3 +2936,22 @@ for (const num of range) {
                 }
 
                 export default App;
+
+
+
+                import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+                function App() {
+                    return (
+                        <BrowserRouter>
+                            <nav>
+                                <Link to="/">Home</Link> | <Link to="/about">About</Link>
+                            </nav>
+                            <Routes>
+                                <Route path="/" element={<h1>Home Page</h1>} />
+                                <Route path="/about" element={<h1>About</h1>} />
+                            </Routes>
+                        </BrowserRouter>
+                    );
+                }
+                export default App;
