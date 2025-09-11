0bea5a83b21393b1aad39dac7d502c7f9ea28fc5
TedGC <rhkdtn2056@gmail.com>
2025-09-11 22:31:15 +0900

    update

--- OnlineMallPR/practice.js
+++ OnlineMallPR/practice.js
@@ -2891,3 +2891,7 @@ for (const num of range) {
 
                     return debounced;
                 }
+
+                app.get("/api/users", (req, res) => {
+                    res.json([{ id: 1, name: "Alice" }, { id: 2, name: "Bob" }]);
+                });
